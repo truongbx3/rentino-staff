b120751517795274c2dde6d9d3c49db77d9c68f6
unknown <truongcntt2@gmail.com>
2026-04-15 23:11:47 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-   apiUrl: 'http://localhost:8888',
-//  apiUrl: 'http://72.60.111.172:8878',
+//  apiUrl: 'http://localhost:8888',
+   apiUrl: 'http://72.60.111.172:8878',
   contextPath: '/api/v1/rentino',
   authContextPath: '/api/v1/auth',
 };
