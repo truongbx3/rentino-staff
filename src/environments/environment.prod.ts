da90386e762e4527eb0f7f445c24c6cca390782c
truongbx3 <Mydream@1234>
2026-08-02 10:37:12 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://swap.tnmgroup.vn',
-  apiUrl: 'http://localhost:8888',
+  apiUrl: 'https://swap.tnmgroup.vn',
+  //apiUrl: 'http://localhost:8888',
   contextPath: '/api/v1/rentino',
   authContextPath: '/api/v1/auth',
 };
